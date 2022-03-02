c655baf8a43c4b1544ae24a2e6038bec318b37de
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-03-02 14:46:35 -0500

    Ajout de code pour faire fonctionner le code avec ghpages take 2

--- src/index.jsx
+++ src/index.jsx
@@ -6,7 +6,7 @@ import {BrowserRouter} from 'react-router-dom';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/panier-achats'>
       <App />
     </BrowserRouter>
   </React.StrictMode>,

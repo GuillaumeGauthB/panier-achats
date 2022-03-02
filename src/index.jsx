c8493bd19e5b94e67c7d9f3f0c96bbff7a42779e
Guillaume Gauthier-Benoit <guillaume.gb@live.ca>
2022-03-02 14:56:42 -0500

    Ajout de code pour faire fonctionner le code avec ghpages version finale

--- src/index.jsx
+++ src/index.jsx
@@ -4,9 +4,12 @@ import './index.scss';
 import App from './App';
 import {BrowserRouter} from 'react-router-dom';
 
+// changer la valeur de basename dependement du url de la page avec href (pour tester l'url entiere) ou hostname (pour juste le nom du host)
+const baseName = (window.location.hostname.search(/github\.io/i) != -1) ? '/panier-achats' : '';
+
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter basename='/panier-achats'>
+    <BrowserRouter basename={baseName}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
